f95fb955f6345e40e896559b047bd4323970512d
Eghosa Ordia <eghordia130@gmail.com>
2025-02-16 15:41:28 +0100

    tab switch logic vollständig

--- src/components/analyticsNavbar.tsx
+++ src/components/analyticsNavbar.tsx
@@ -1,33 +1,40 @@
 import React from "react";
 
-const AnalyticsNavbar: React.FC = () => {
-  const secnavbtn = [
-    {
-      txt: "Statistics Dashboard",
-      icon: "lni lni-trend-up-1 my-auto",
-      status: "not-active",
-    },
-    {
-      txt: "Trade History",
-      icon: "lni lni-clipboard my-auto",
-      status: "not-active",
-    },
-    {
-      txt: "Custom Reports",
-      icon: "lni lni-file-multiple my-auto",
-      status: "not-active",
-    },
-  ];
+interface Props {
+  activeTab: string;
+  setActiveTab: (tab: string) => void;
+}
+
+const secnavbtn = [
+  {
+    id: "dashboard",
+    txt: "Statistics Dashboard",
+    icon: "lni lni-trend-up-1 my-auto",
+  },
+  {
+    id: "history",
+    txt: "Trade History",
+    icon: "lni lni-clipboard my-auto",
+  },
+  {
+    id: "report",
+    txt: "Custom Reports",
+    icon: "lni lni-file-multiple my-auto",
+  },
+];
+
+const AnalyticsNavbar: React.FC<Props> = ({ activeTab, setActiveTab }) => {
   return (
     <>
       <div className="sec-nav bg-dark-2 border border-grey-3 rounded-xl p-3">
         <ul className="navlinks rounded-md text-grey-2 font-bold text-xs text-center grid grid-cols-3 bg-dark-1 p-1">
-          {secnavbtn.map(({ txt, icon }, index) => (
+          {secnavbtn.map(({ id, txt, icon }, index) => (
             <li key={index}>
               <button
                 className={`sec-nav-btn cursor-pointer ${
-                  txt == "Statistics Dashboard" ? "bg-light-1 text-black" : ""
+                  txt == "dashboard" ? "bg-light-1 text-black" : ""
                 }`}
+                onClick={() => setActiveTab(id)}
               >
                 <i className={icon}></i>
                 <span className="my-auto">{txt}</span>
--- src/pages/analytics.tsx
+++ src/pages/analytics.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 import AsidesSection from "../components/aside";
 import NavBar from "../components/navbar";
@@ -7,15 +7,16 @@ import AnalyticsDashboard from "../components/analyticsDashboard";
 import TradeHistory from "../components/tradeHistory";
 
 const Analytics: React.FC = () => {
+  const [activeTab, setActiveTab] = useState("dashboard");
   return (
     <>
       <AsidesSection />
       <main className="min-h-dvh ml-12 w-full bg-dark-1 overflow-hidden">
         <NavBar />
         <div className="body p-8 grid gap-8">
-          <AnalyticsNavbar />
-          <AnalyticsDashboard />
-          <TradeHistory />
+          <AnalyticsNavbar activeTab={activeTab} setActiveTab={setActiveTab} />
+          {activeTab === "dashboard" && <AnalyticsDashboard />}
+          {activeTab === "history" && <TradeHistory />}
         </div>
       </main>
     </>
