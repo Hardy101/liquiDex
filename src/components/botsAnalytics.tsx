cbac1a75884d3a6161ca841ee76a93e39e65485d
Eghosa Ordia <eghordia130@gmail.com>
2025-02-21 13:26:09 +0100

    rename custom hook

--- src/components/botsAnalytics.tsx
+++ src/components/botsAnalytics.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import cryptoDataHandler from "../services/cryptoDataHandler";
+import useCryptoData from "../services/useCryptoData";
 
 interface FormData {
   ticker: string;
@@ -9,19 +9,35 @@ const BotAnalytics: React.FC = () => {
   const [formData, setFormData] = useState<FormData>({ ticker: "" });
   const [coinId, setCoinId] = useState("");
   const [days, setDays] = useState("1");
-  const { data, loading, error } = cryptoDataHandler(coinId, days);
+  const [submittedCoin, setSubmittedCoin] = useState<string>(coinId);
+  const [submittedDays, setSubmittedDays] = useState<string>(days);
+
+  const { coindata, loading, error } = useCryptoData(
+    submittedCoin,
+    submittedDays
+  );
+
+  // const [submittedData, setSubmittedData] = useState<any>(null);
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>): void => {
     const { name, value } = e.target;
     setCoinId(value);
     setFormData({ ...formData, [name]: value });
   };
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    setSubmittedCoin(coinId);
+    setSubmittedDays(days);
+  };
   return (
     <>
       <div className="text-light-1 text-xs flex justify-between gap-16">
         <form
+          method="get"
           action="#"
           className="w-1/2 flex flex-col gap-2 text-xs bg-dark-2 border border-grey-3 rounded-xl p-3"
+          onSubmit={handleSubmit}
         >
           <h2 className="text-xl font-bold">Asset Analysis</h2>
           <div className="relative form-control flex justify-between pb-2">
@@ -83,58 +99,82 @@ const BotAnalytics: React.FC = () => {
           </div>
           <div className="form-control flex justify-between pb-2">
             <button
-              type="button"
-              className="w-full gradient-1 p-2 rounded-md text-black"
+              type="submit"
+              className="w-full gradient-1 p-2 rounded-md text-black hover:scale-105 transition duration-300 ease-in-out"
             >
               Analyse Ticker
             </button>
           </div>
         </form>
         <div className="w-1/2 bg-dark-2 border border-grey-3 rounded-xl p-3">
-          {loading && <p>Loading historical data...</p>}
+          {loading && (
+            <p className="text-grey-4 text-xl p-4">
+              Enter ticker to analyse...
+            </p>
+          )}
           {error && <p className="text-red-500">{error}</p>}
-          {data && (
+          {coindata && !loading && !error && (
             <div>
               <h2 className="text-xl mb-2">{coinId}</h2>
               <p className="flex gap-4">
-                <button className="flex gap-2">
-                  <span className="bg-dark-1 py-1 px-2 border border-grey-3 rounded-md text-green-500 my-auto">
-                    <i className="fa-solid fa-arrow-up"></i>
+                <span className="flex gap-2">
+                  <span
+                    className={`bg-dark-1 py-1 px-2 border border-grey-3 rounded-md text-${
+                      coindata[0] > 3 ? "green-500" : "red-500"
+                    } my-auto`}
+                  >
+                    <i
+                      className={`fa-solid fa-arrow-${
+                        coindata[0] > 3 ? "up" : "down"
+                      }`}
+                    ></i>
                   </span>
-                  <span className="my-auto">Long</span>
-                </button>
-                <button className="flex gap-2">
+                  <span className="my-auto">
+                    {coindata[0] > 3 ? "long" : "short"}
+                  </span>
+                </span>
+                <span className="flex gap-2">
                   <span className="bg-dark-1 py-1 px-2 border border-grey-3 rounded-md">
                     <i className="fa-solid fa-bullseye"></i>
                   </span>
                   <span className="my-auto">30-50%</span>
-                </button>
+                </span>
               </p>
               <div className="mt-4 relative flex gap-4">
                 <span className="bg-dark-1 py-1 px-2 border border-grey-3 rounded-md text-red-400">
                   <i className="fa-solid fa-triangle-exclamation my-auto"></i>
                 </span>
                 <div className="w-full h-2 bg-blue-300 rounded-md my-auto"></div>
-                <span className="my-auto font-bold">70%</span>
+                <span className="my-auto font-bold">
+                  {coindata[1].toFixed(2)}%
+                </span>
               </div>
               <div className="mt-4 relative flex gap-4">
                 <button className="bg-dark-1 py-1 px-2 border border-grey-3 rounded-md text-blue-400">
                   <i className="fa-solid fa-dice-two my-auto"></i>
                 </button>
-                <div className="w-1/2 h-2 bg-blue-300 rounded-md my-auto"></div>
-                <span className="my-auto font-bold">50%</span>
+                <div
+                  className={`w-1/2 ${
+                    Number(coindata[2].toFixed(0)) > 50
+                      ? "bg-blue-300"
+                      : "bg-red-300"
+                  } h-2 rounded-md my-auto`}
+                ></div>
+                <span className="my-auto font-bold">
+                  {coindata[2].toFixed(0)}%
+                </span>
               </div>
               <div className="mt-6 text-md flex gap-4">
                 <p className="flex gap-2">
                   <span className="my-auto">Take Profit</span>
                   <span className="my-auto bg-dark-1 py-1 px-2 border border-grey-3 rounded-md">
-                    $5.9
+                    ${coindata[3].toFixed(4)}
                   </span>
                 </p>
                 <p className="flex gap-2">
                   <span className="my-auto">Stop Loss</span>
                   <span className="my-auto bg-dark-1 py-1 px-2 border border-grey-3 rounded-md">
-                    $2.9
+                    ${coindata[4].toFixed(4)}
                   </span>
                 </p>
               </div>
@@ -187,19 +227,3 @@ export default BotAnalytics;
 // │   └── vite.config.js        # If using Vite (recommended for React speed)
 // │
 // └── README.md
-{
-  /* <div className="absolute w-3/5 right-0 top-10 rounded-md bg-black border border-grey-3">
-<div className="flex flex-col gap-2">
-  <p className="grid grid-cols-3 px-3 py-1 bg-dark-1">
-    <span>ID</span>
-    <span>Symbol</span>
-    <span>Name</span>
-  </p>
-  <p className="grid grid-cols-3 px-3 py-1">
-    <span>BTC</span>
-    <span>BTC</span>
-    <span>Bitcoin</span>
-  </p>
-</div>
-</div> */
-}
