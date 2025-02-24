978cec8f8c35fddb2ed0541b4d996b0854651a6d
Eghosa Ordia <eghordia130@gmail.com>
2025-02-24 15:30:28 +0100

    notifications schema complete

--- backend/models/schemas.py
+++ backend/models/schemas.py
@@ -1,4 +1,5 @@
 from pydantic import BaseModel
+from sqlalchemy import Boolean
 from sqlmodel import SQLModel, Field
 from typing import Optional
 from datetime import datetime
@@ -14,3 +15,11 @@ class AnalysisResult(BaseModel):
     probability: float
     take_profit: float
     stop_loss: float
+
+class Notification(SQLModel, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
+    title: str
+    message: str
+    inapp_type: Optional[bool] = Field(default=False)
+    emaii_type: Optional[bool] = Field(default=False)
+    created_at: datetime = Field(default_factory=datetime.now)
