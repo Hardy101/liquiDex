cf5f4549135281a2553717db7969fb16ddb07b57
Eghosa Ordia <eghordia130@gmail.com>
2025-02-24 17:21:12 +0100

    moved code up in file

--- backend/routers/notifications.py
+++ backend/routers/notifications.py
@@ -5,10 +5,10 @@ from services.notifications import get_notifications, create_notification
 
 router = APIRouter()
 
-@router.get('/')
-def read_notifications(session: Session = Depends(get_session)):
-    return get_notifications(session)
-
 @router.post("/")
 def add_notification(title: str, message: str, inapp, email_type, session: Session = Depends(get_session)):
     return create_notification(session, title, message, inapp, email_type)
+
+@router.get('/')
+def read_notifications(session: Session = Depends(get_session)):
+    return get_notifications(session)
