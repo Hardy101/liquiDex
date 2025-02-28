3ad4ed877ddd98cb84665a3476167cd4ba56a180
Eghosa Ordia <eghordia130@gmail.com>
2025-02-28 17:11:39 +0100

    authentication page design

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -7,12 +7,12 @@ import Analytics from "./pages/analytics";
 import Settings from "./pages/settings";
 import Notifications from "./pages/notifications";
 import ComingSoonPage from "./pages/comingSoon";
+import Authentication from "./pages/authentication";
 
 function App() {
   return (
     <>
       <Router>
-        //{" "}
         <Routes>
           <Route path="/" element={<Dashboard />} />
           <Route path="/bots" element={<Bots />} />
@@ -21,6 +21,7 @@ function App() {
           <Route path="/notifications" element={<Notifications />} />
           <Route path="/wallet" element={<ComingSoonPage />} />
           <Route path="/database" element={<ComingSoonPage />} />
+          <Route path="/auth" element={<Authentication />} />
         </Routes>
       </Router>
     </>
--- frontend/src/pages/authentication.tsx
+++ frontend/src/pages/authentication.tsx
@@ -1,5 +1,6 @@
 import AsidesSection from "../components/aside";
 import NavBar from "../components/navbar";
+import { Link } from "react-router-dom";
 
 const Authentication: React.FC = () => {
   return (
@@ -7,8 +8,25 @@ const Authentication: React.FC = () => {
       <AsidesSection />
       <main className="min-h-dvh ml-12 w-full bg-dark-1 overflow-hidden">
         <NavBar />
-        <div className="body p-8 grid gap-8">
-          <div className="h-full text-light-1"></div>
+        <div className="body p-8 grid gap-8 p-8">
+          <div className="h-full text-light-1 bg-dark-2 border border-grey-3 rounded-xl p-3 text-light-1 text-xs">
+            <h1 className="text-2xl font-bold">Welcome back</h1>
+            <p className="text-grey-4">
+              Do you not have an account? then <Link to="#">sign up</Link>
+            </p>
+            <p>
+              <button>
+                <span>Email Address</span>
+                <span>Use personal email address</span>
+              </button>
+            </p>
+            <p className="mt-4">
+              <button className="flex gap-4 bg-dark-1 rounded-md p-2">
+                <i className="fa-brands fa-google my-auto"></i>
+                <span className="my-auto">Authenticate with Google</span>
+              </button>
+            </p>
+          </div>
         </div>
       </main>
     </>
